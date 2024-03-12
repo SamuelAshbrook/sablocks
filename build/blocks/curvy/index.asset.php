1e403456f5755f54d92ddd7998f4cfdc1fab1961
SamuelAshbrook <ashbrook444@gmail.com>
2024-03-12 16:58:33 +0000

    Adding additional options to curvy block

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '856acd6c0215aa981d5d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2c2c9c3f1f942ecda825');
